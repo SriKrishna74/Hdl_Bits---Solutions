beb6b279e3a85972d74993c94d3b479f12ed1a72
SRI KRISHNA G <127392900+SriKrishna74@users.noreply.github.com>
2023-07-10 22:22:55 +0530

    Create Vector0.v

---
+++ VECTORS/Vector0.v
@@ -0,0 +1,32 @@
+module top_module ( 
+    input wire [2:0] vec,
+    output wire [2:0] outv,
+    output wire o2,
+    output wire o1,
+    output wire o0); // Module body starts after module declaration
+    assign outv[2:0]=vec[2:0];
+    assign o2=vec[2];
+    assign o1=vec[1];
+    assign o0=vec[0];
+
+endmodule
+
+
+// their solution
+
+module top_module(
+	input [2:0] vec, 
+	output [2:0] outv,
+	output o2,
+	output o1,
+	output o0
+);
+	
+	assign outv = vec;
+
+	// This is ok too: assign {o2, o1, o0} = vec;
+	assign o0 = vec[0];
+	assign o1 = vec[1];
+	assign o2 = vec[2];
+	
+endmodule
