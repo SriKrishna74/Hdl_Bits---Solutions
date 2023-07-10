205007a46865cc1989585de4ad91733089821d23
SRI KRISHNA G <127392900+SriKrishna74@users.noreply.github.com>
2023-07-10 22:37:19 +0530

    Create Vector5.v

---
+++ VECTORS/Vector5.v
@@ -0,0 +1,30 @@
+// MY SOLUTION
+
+module top_module (
+    input a, b, c, d, e,
+    output [24:0] out );//
+
+    // The output is XNOR of two vectors created by 
+    // concatenating and replicating the five inputs.
+    assign out[24:0] = ~{ {5{a}},{5{b}},{5{c}},{5{d}},{5{e}}} ^ { {5{a,b,c,d,e}}};
+                        
+                       
+
+endmodule
+
+// THEIR SOLUTION
+
+module top_module (
+	input a, b, c, d, e,
+	output [24:0] out
+);
+
+	wire [24:0] top, bottom;
+	assign top    = { {5{a}}, {5{b}}, {5{c}}, {5{d}}, {5{e}} };
+	assign bottom = {5{a,b,c,d,e}};
+	assign out = ~top ^ bottom;	// Bitwise XNOR
+
+	// This could be done on one line:
+	// assign out = ~{ {5{a}}, {5{b}}, {5{c}}, {5{d}}, {5{e}} } ^ {5{a,b,c,d,e}};
+	
+endmodule
